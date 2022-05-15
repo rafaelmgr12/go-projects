f3b8b72bce921d4c32453cade1dd3bee6abed034
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-05-15 20:35:12 -0300

    finish third module: functions in go

--- intro-go/src/app-go/app.go
+++ intro-go/src/app-go/app.go
@@ -1,18 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+
+	showInfo()
+	showMenu()
+	option := readOption()
+
+	// if option == 1 {
+	// 	fmt.Println("Start Monitoring")
+	// } else if option == 2 {
+	// 	fmt.Println("Show Logs")
+	// } else if option == 0 {
+	// 	fmt.Println("Exit")
+	// } else {
+	// 	fmt.Println("Invalid Option")
+	// }
+	// it is possible to use switch case in Go
+	switch option {
+	case 1:
+		fmt.Println("Monitoring...")
+	case 2:
+		fmt.Println("Show Logs...")
+	case 0:
+		fmt.Println("Exiting...")
+		os.Exit(0)
+	default:
+		fmt.Println("Invalid Option")
+		os.Exit(-1)
+	}
+
+}
+
+func showInfo() {
 	name := "Rafael"
 	version := 1.1
 
 	fmt.Println("Hello, ", name, "!")
 	fmt.Println("The program is in the version ", version)
+}
 
+func showMenu() {
 	fmt.Println("1- Start Monitoring")
 	fmt.Println("2- Show Logs")
-	fmt.Println("3- Exit")
+	fmt.Println("0- Exit")
 
+}
+
+func readOption() int {
 	var option int
 	//fmt.Scanf("%d", &option)
 	// it is possible to use anothe way to read the option,
@@ -20,4 +59,5 @@ func main() {
 	fmt.Scan(&option)
 
 	fmt.Println("You selected option ", option)
+	return option
 }
--- intro-go/src/variables-in-go/hello.go
+++ intro-go/src/variables-in-go/hello.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Rafael"
-	var idade int = 27
-	var versao float32 = 1.2
+	// var nome string = "Rafael"
+	// var idade int = 27
+	// var versao float32 = 1.2
+
+	// go can infer variables types
+
+	// var nome = "Rafael"
+	// var idade = 27
+	// var versao = 1.2
+
+	// go can also take out the var name and still work
+
+	nome := "Rafael"
+	idade := 27
+	versao := 1.2
 
 	fmt.Println("Olá, sr.", nome, "sua	idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
